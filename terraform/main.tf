ed12e7c82d4538d05370cc7b7cf8c6fb4614af02
Xavier Basty-Kjellberg <xavier@walletconnect.com>
2023-01-19 15:22:24 +0100

    feat: add `DocumentDB` instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,17 +4,17 @@ module "label" {
 }
 
 locals {
-  app_name            = "cast-server"
-  fqdn                = terraform.workspace == "prod" ? var.public_url : "${terraform.workspace}.${var.public_url}"
-  latest_release_name = data.github_release.latest_release.name
-  version             = coalesce(var.image_version, substr(local.latest_release_name, 1, length(local.latest_release_name))) # tflint-ignore: terraform_unused_declarations
+  app_name = "cast-server"
+  fqdn     = terraform.workspace == "prod" ? var.public_url : "${terraform.workspace}.${var.public_url}"
+  # latest_release_name = data.github_release.latest_release.name
+  # version             = coalesce(var.image_version, substr(local.latest_release_name, 1, length(local.latest_release_name))) # tflint-ignore: terraform_unused_declarations
 }
 
-data "github_release" "latest_release" {
-  repository  = local.app_name
-  owner       = "walletconnect"
-  retrieve_by = "latest"
-}
+# data "github_release" "latest_release" {
+#   repository  = local.app_name
+#   owner       = "walletconnect"
+#   retrieve_by = "latest"
+# }
 
 module "dns" {
   source = "github.com/WalletConnect/terraform-modules.git//modules/dns" # tflint-ignore: terraform_module_pinned_source
